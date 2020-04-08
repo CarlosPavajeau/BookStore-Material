fa78dfc21c3828cd32740244f8005b5cee1733d6
Carlos Pavajeau <cantte098@gmail.com>
2020-04-08 09:08:53 -0500

    FrontEnd: Add app-routing module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -10,6 +10,15 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OffersComponent } from './components/offers/offers.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BooksComponent } from './components/admin/books/books.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +26,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    OffersComponent,
+    HeroComponent,
+    BookDetailsComponent,
+    BooksComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +45,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
