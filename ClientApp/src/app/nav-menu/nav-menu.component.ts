ffa7979f1803733f84250f9c7b3a8a363e925a1e
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 09:00:49 -0500

    FrontEnd: Make new navigation bar

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnInit {
 
-  collapse() {
-    this.isExpanded = false;
+  public isLogged: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.onCheckUser();
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  onLogout(): void {
+    this.authService.logoutUser();
+    this.isLogged = false;
+    window.location.reload();
+  }
+
+  onCheckUser(): void {
+    if (this.authService.getCurrentUser()) {
+      this.isLogged = true;
+    }
+    else {
+      this.isLogged = false;
+    }
   }
 }
