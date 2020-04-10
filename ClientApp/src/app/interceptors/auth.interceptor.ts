06041c8edef1cedc7b0ce9eaa5cea7f41eb3d7f5
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 11:33:46 -0500

    FrontEnd: Make auth interceptor

--- ClientApp/src/app/interceptors/auth.interceptor.ts
+++ ClientApp/src/app/interceptors/auth.interceptor.ts
@@ -6,13 +6,40 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.authService.getCurrentUser()) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Beared ${this.authService.getToken()}`
+        }
+      });
+
+      return next.handle(request)
+        .pipe(
+          tap(
+            succ => { },
+            err => {
+              if (err.status == 401) {
+                this.authService.removeUser();
+                this.router.navigateByUrl('/user/login');
+              }
+            }
+          )
+        );
+    } else {
+      return next.handle(request);
+    }
   }
 }
