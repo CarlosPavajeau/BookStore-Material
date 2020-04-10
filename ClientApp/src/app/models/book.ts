06f7d62106ce4778cdee74952f73b3d97331320f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 08:35:31 -0500

    FrontEnd: Add app models book and user

---
+++ ClientApp/src/app/models/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  title?: string;
+  language?: string;
+  description?: string;
+  coverPage?: string;
+  price?: number;
+  amazonLink?: string;
+  author?: string;
+  id?: string;
+  offer?: boolean;
+}
