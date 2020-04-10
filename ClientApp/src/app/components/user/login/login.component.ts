abd814315ff8036f746e20f6cbc3fc83eacff417
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 09:06:01 -0500

    FrontEnd/User: Make complete user components

--- ClientApp/src/app/components/user/login/login.component.ts
+++ ClientApp/src/app/components/user/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location } from "@angular/common";
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  invaidUserOrPassword: boolean = false;
+  invalidForm: boolean = false;
+
+  public get controls() {
+    return this.loginForm.controls;
+  }
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private location: Location
+    ) { }
 
   ngOnInit(): void {
+    if (this.authService.getCurrentUser()) {
+      this.router.navigateByUrl('/user/profile');
+    } else {
+      this.loginForm = this.formBuilder.group({
+        username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]]
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      let user: User = {
+        name: this.controls['username'].value,
+        password: this.controls['password'].value
+      };
+
+      this.authService.loginUser(user).subscribe(user => {
+        if (user) {
+          this.authService.setUser(user);
+          window.location.reload();
+        } else {
+          this.invaidUserOrPassword = true;
+          setTimeout(() => {
+            this.invaidUserOrPassword = false;
+          }, 4000);
+        }
+      });
+    } else {
+      this.invalidForm = true;
+    }
   }
 
 }
--- ClientApp/src/app/components/user/register/register.component.ts
+++ ClientApp/src/app/components/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from "../../../services/auth.service";
+import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  invalidForm: boolean;
+
+  public get controls() {
+    return this.userForm.controls;
+  }
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      let user: User = {
+        name: this.controls['userName'].value,
+        email: this.controls['email'].value,
+        password: this.controls['password'].value
+      };
+
+      this.authService.registerUser(user)
+        .subscribe(user => {
+          console.log(user);
+          if (user) {
+            this.onReset();
+            this.authService.setUser(user);
+            this.router.navigateByUrl(`/profile`);
+          }
+        });
+
+    } else console.log('Invalid form');
+  }
+
+  onReset() {
+
   }
 
 }
