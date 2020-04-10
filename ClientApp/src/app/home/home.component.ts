0ec43a52942be8e341508fb061b83d801c64e143
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 09:03:25 -0500

    FrontEnd: Make new home page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from "../models/book";
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  books: Book[];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.getBooks()
+  }
+
+  getBooks() {
+    this.bookService.getBooks().subscribe(
+      books => this.books = books
+    );
+  }
+
 }
