5ae0834f4982a1c1578a4ed8b0591f793211e86c
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 10:49:32 -0500

    FrontEnd: Add MatToolTipModule

--- ClientApp/src/app/material.module.ts
+++ ClientApp/src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatChipsModule } from '@angular/material/chips';
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 
 
@@ -33,7 +34,8 @@ import { MatPaginatorModule } from "@angular/material/paginator";
     MatDialogModule,
     MatCheckboxModule,
     MatChipsModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatTooltipModule
   ],
   exports: [
     MatButtonModule,
@@ -49,7 +51,8 @@ import { MatPaginatorModule } from "@angular/material/paginator";
     MatDialogModule,
     MatCheckboxModule,
     MatChipsModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
