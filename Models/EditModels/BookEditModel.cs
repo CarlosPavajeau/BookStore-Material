9264e365b1cef8b676757911796476d782adf60f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 08:41:59 -0500

    BackEnd/InputModels: Add user and book edit model

---
+++ Models/EditModels/BookEditModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.EditModels
+{
+    public class BookEditModel
+    {
+        public string Title { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string CoverPage { get; set; }
+        public decimal Price { get; set; }
+        public Uri AmazonLink { get; set; }
+        public string Author { get; set; }
+        public bool Offer { get; set; }
+    }
+}
