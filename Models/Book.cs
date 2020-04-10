55a90501e9cf73aae7ff03e1d06397651568da0e
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 08:41:00 -0500

    BackEnd/Models: Add book model

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string CoverPage { get; set; }
+        public decimal Price { get; set; }
+        public Uri AmazonLink { get; set; }
+        public string Author { get; set; }
+        public bool Offer { get; set; }
+    }
+}
