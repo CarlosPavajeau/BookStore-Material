e847eb3b5bd1bae6f217e58274632c27be7d9855
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 09:10:00 -0500

    FrontEnd/Offers: Make a complete component

--- ClientApp/src/app/components/offers/offers.component.ts
+++ ClientApp/src/app/components/offers/offers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-offers',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersComponent implements OnInit {
 
-  constructor() { }
+  books: Book[];
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.bookService.getOffers()
+      .subscribe(books => this.books = books);
   }
 
 }
