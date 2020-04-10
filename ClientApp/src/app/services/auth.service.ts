76a0b32ccebbc495b499fb46497bc49c79f4c932
Carlos Pavajeau <cantte098@gmail.com>
2020-04-10 08:36:27 -0500

    FrontEnd: Add app services, auth and book service

---
+++ ClientApp/src/app/services/auth.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap, catchError } from "rxjs/operators";
+import { isNullOrUndefined } from "util";
+import { HttpErrorHandlerService } from '../@base/http-error-handler.service';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  readonly authUrl = 'api/Users';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient, private httpErrorHandler: HttpErrorHandlerService) { }
+
+  registerUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.authUrl}`, user, this.httpOptions)
+      .pipe(
+        tap(_ => this.httpErrorHandler.log('User registered')),
+        catchError(this.httpErrorHandler.handleError<User>('registerUser', null))
+      );
+  }
+
+  loginUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.authUrl}/Login`, user, this.httpOptions)
+      .pipe(
+        tap(_ => this.httpErrorHandler.log('User try login')),
+        catchError(this.httpErrorHandler.handleError<User>('loginUser', null))
+      );
+  }
+
+  setUser(user: User): void {
+    let user_string = JSON.stringify(user);
+    localStorage.setItem('currentUser', user_string);
+  }
+
+  getCurrentUser(): User {
+    let user_string = localStorage.getItem('currentUser');
+    if (!isNullOrUndefined(user_string)) {
+      let user: User = JSON.parse(user_string);
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('accessToken', token);
+  }
+
+  getToken(): string {
+    let user: User = this.getCurrentUser();
+
+    if (user) {
+      return user.token;
+    } else {
+      return '';
+    }
+  }
+
+  logoutUser(): Observable<User> {
+    let currentUser: User = this.getCurrentUser();
+    let accessToken = currentUser.token;
+    localStorage.removeItem('currentUser');
+    return this.http.post<User>(`${this.authUrl}/Logout?access_token=${accessToken}`, this.httpOptions)
+      .pipe(
+        tap(_ => this.httpErrorHandler.log('User logout')),
+        catchError(this.httpErrorHandler.handleError<User>('logoutUser', null))
+      );
+  }
+}
---
+++ ClientApp/src/app/services/book.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { tap, catchError } from "rxjs/operators";
+import { Book } from '../models/book';
+import { HttpErrorHandlerService } from '../@base/http-error-handler.service';
+import { ServerResponse } from '../@base/server-response';
+import { AuthService } from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  readonly bookUrl = 'api/Books';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.authService.getToken()
+    })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private httpErrorHandler: HttpErrorHandlerService,
+    private authService: AuthService
+    ) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.bookUrl}`, this.httpOptions)
+      .pipe(
+        tap(_ => this.httpErrorHandler.log('Books receive'),
+        catchError(this.httpErrorHandler.handleError<Book[]>('getBooks', [])))
+      );
+  }
+
+  getBook(id: string): Observable<Book> {
+    return this.http.get<Book>(`${this.bookUrl}/${id}`, this.httpOptions)
+      .pipe(
+        tap(_ => this.httpErrorHandler.log('Book receive')),
+        catchError(this.httpErrorHandler.handleError<Book>('getBook', null))
+      );
+  }
+
+  getOffers(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.bookUrl}/Offers`, this.httpOptions)
+      .pipe(
+        tap(_ => this.httpErrorHandler.log('Offers receive')),
+        catchError(this.httpErrorHandler.handleError<Book[]>('getOffers', []))
+      );
+  }
+
+  saveBook(book: Book): Observable<Book> {
+    let token = this.authService.getToken();
+    return this.http.post<Book>(`${this.bookUrl}?access_token=${token}`, book, this.httpOptions)
+      .pipe(
+        tap(_ => console.log('Book saved')),
+        catchError(this.httpErrorHandler.handleError<Book>('saveBook', null))
+      );
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    let token = this.authService.getToken();
+    return this.http.put<Book>(`${this.bookUrl}/${book.id}?access_token=${token}`, book, this.httpOptions)
+      .pipe(
+        tap(_ => console.log('Book Updated')),
+        catchError(this.httpErrorHandler.handleError<Book>('updateBook', null))
+      );
+  }
+
+  deleteBook(id: string): Observable<Book> {
+    let token = this.authService.getToken();
+    return this.http.delete<Book>(`${this.bookUrl}/${id}?access_token=${token}`, this.httpOptions)
+      .pipe(
+        tap(_ => console.log('Book deleted')),
+        catchError(this.httpErrorHandler.handleError<Book>('deleteBook', null))
+      );
+  }
+}
