799491d8ad7468e84a687dc02ddabfe7a1dc3fa9
Carlos Pavajeau <cantte098@gmail.com>
2020-04-12 10:50:26 -0500

    BackEnd/Migrations: Add Auth migrations

--- Migrations/20200412143043_Auth.Designer.cs
+++ Migrations/20200412143043_Auth.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BookStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200412143043_Auth")]
+    partial class Auth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
